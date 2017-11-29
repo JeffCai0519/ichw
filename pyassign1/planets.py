35f32e98aa12c910febf41acca8962dac3176d2f
jeffcai0519 <jiajicaiyp@pku.edu.cn>
2017-11-29 21:31:15 +0800

    py1FINAL

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 """planets.py: Draws ecliptic orbits of 6 planets around the sun.
-
 __author__ = "Cai Jiaji"
 __pkuid__ = "1700017797"
 __email__ = "jiajicaiyp@pku.edu.cn"
@@ -11,34 +10,15 @@ import turtle
 import math
 
 
-def eclipse(t, a, b, s, o, n):
+def place(t, a, b, o):
     c = math.sqrt(a**2-b**2)
-    circumference = math.pi*(3/2*(a+b)-math.sqrt(a*b))
-    step = circumference/n
-
     t.up()
     if o == "left":
         t.setpos(a+c, 0)
     if o == "right":
         t.setpos(a-c, 0)
-
     t.down()
     t.setheading(90)
-    t.speed(s)
-    t.forward(step)
-
-    for j in range(4):
-        for i in range(n):
-            p = t.position()
-            x = p[0]
-            y = p[1]
-            if o == "left":
-                angle_rad = math.pi + math.atan2(-b**2*(x-c/2), a**2*y)
-            if o == "right":
-                angle_rad = math.pi + math.atan2(-b**2*(x+c/2), a**2*y)
-            angle = math.degrees(angle_rad)
-            t.setheading(angle)
-            t.forward(step)
 
 
 def main():
@@ -50,13 +30,9 @@ def main():
     jupiter = turtle.Turtle()
     saturn = turtle.Turtle()
 
-    sun.shape("circle")
-    mercury.shape("circle")
-    venus.shape("circle")
-    earth.shape("circle")
-    mars.shape("circle")
-    jupiter.shape("circle")
-    saturn.shape("circle")
+    for t in [sun, mercury, venus, earth, mars, jupiter, saturn]:
+        t.shape("circle")
+        t.speed(0)
 
     sun.color("yellow")
     mercury.color("blue")
@@ -66,12 +42,44 @@ def main():
     jupiter.color("orange")
     saturn.color("cyan")
 
-    eclipse(mercury, 30, 30, 0, "left", 1000)
-    eclipse(venus, 50, 40, 0, "left", 1000)
-    eclipse(mars, 100, 75, 0, "right", 1000)
-    eclipse(earth, 125, 50, 0, "left", 1000)
-    eclipse(jupiter, 150, 30, 0, "left", 1000)
-    eclipse(saturn, 175, 100, 0, "left", 1000)
+    place(mercury, 30, 30, "left")
+    place(venus, 50, 40, "left")
+    place(mars, 100, 75, "right")
+    place(earth, 125, 50, "left")
+    place(jupiter, 150, 30, "left")
+    place(saturn, 175, 100, "left")
+
+    for t in [sun, mercury, venus, earth, mars, jupiter, saturn]:
+        t.forward(0.01)
+
+    for j in range(4):
+        for i in range(1000):
+            for t in [mercury, venus, earth, mars, jupiter, saturn]:
+                if t == mercury:
+                    a, b = 30, 30
+                if t == venus:
+                    a, b = 50, 40
+                if t == earth:
+                    a, b = 100, 75
+                if t == mars:
+                    a, b = 125, 50
+                if t == jupiter:
+                    a, b = 150, 30
+                if t == saturn:
+                    a, b = 175, 100
+                c = math.sqrt(a**2-b**2)
+                circumference = math.pi*(3/2*(a+b)-math.sqrt(a*b))
+                step = circumference/1000
+                p = t.position()
+                x = p[0]
+                y = p[1]
+                if t == mars:
+                    angle_rad = math.pi + math.atan2(-b**2*(x+c/2), a**2*y)
+                else:
+                    angle_rad = math.pi + math.atan2(-b**2*(x-c/2), a**2*y)
+                angle = math.degrees(angle_rad)
+                t.setheading(angle)
+                t.forward(step)
 
     turtle.exitonclick()
 
